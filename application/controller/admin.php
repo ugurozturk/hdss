caf1d60f00b94fd27af27e6dff16bc6fcc709eb6
Uğur Öztürk <tarantulaugur@hotmail.com>
2016-03-10 01:48:02 +0200

    Admin Controller View ve Resim Yükleme Yapıldı

---
+++ application/controller/admin.php
@@ -0,0 +1,71 @@
+<?php
+
+
+
+class Admin extends Controller
+{
+  public function index()
+  {
+
+/*
+if(isset($_SESSION['uyeGrup'])
+    {
+
+
+    }
+
+*/
+   // load views. within the views we can echo out $songs and $amount_of_songs easily
+    require APP . 'view/_templates/header.php';
+    require APP . 'view/admin/index.php';
+    require APP . 'view/_templates/footer.php';
+  }
+
+  public function yukle(){
+    if(!empty($_FILES['files']['name'][0])){
+      $files = $_FILES['files'];
+
+      $uploaded = array();
+      $failed = array();
+
+      $allowed = array('jpg', 'png', 'gif');
+
+      foreach ($files['name'] as $pozisyon => $file_name) {
+
+        $file_temp = $files['tmp_name'][$pozisyon];
+        $file_size = $files['size'][$pozisyon];
+        $file_error = $files['error'][$pozisyon];
+
+        $file_ext = explode('.', $file_name);
+
+        $file_ext = strtolower(end($file_ext));
+
+        if(in_array($file_ext, $allowed)){
+
+          if($file_size <= 999999){
+            $file_name_new = uniqid('',true) . '.' . $file_ext;
+            $file_destination = 'img/' . $file_name_new;
+
+            if(move_uploaded_file($file_temp, $file_destination)) {
+              $uploaded[$pozisyon] = $file_destination;
+            }
+            else {
+              $failed[$pozisyon] = '[{$file_name}] hata verdi'; 
+            }
+
+          }
+          else {
+
+          }
+
+        }
+        else {
+        }
+
+        if(!empty($uploaded)){
+          print_r($uploaded);
+        }
+      }
+    }
+  }
+}
---
+++ application/view/admin/index.php
@@ -0,0 +1,11 @@
+<div class="container-fluid">
+
+<form action="<?php echo URL . 'admin/yukle'; ?>" method="post" enctype="multipart/form-data" >
+<input type="file" name="files[]" multiple="true" />
+<input type="submit" value="Gönder" />
+
+
+</form>
+
+
+</div>
