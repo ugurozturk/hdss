209b3c5d56ecadd5ac2f514d34049434d0defcdf
Uğur Öztürk <tarantulaugur@hotmail.com>
2016-04-15 13:43:24 +0300

    Admin girişini admin sayfasına yönlendirildi

--- application/controller/login.php
+++ application/controller/login.php
@@ -15,12 +15,14 @@ class Login extends Controller
 
     if(!empty($donen)){
       $_SESSION['uye_grup'] = $donen->user_group_id;
+      if($donen->user_group_id == 1){ header('location: ' . URL.'admin');}
     }
     else {
       $_SESSION['uye_grup'] = null;
+
+          header('location: ' . URL);
     }
 
-    header('location: ' . URL);
 
   }
 
